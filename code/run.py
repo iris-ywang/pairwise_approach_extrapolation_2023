f5f8876e6b31e03290824eb3cc02305a47c3ee2d
Iris Wang <iris_ywang@outlook.com>
2022-10-14 21:54:09 +0100

    trying to figure when did it broke

--- code/run.py
+++ code/run.py
@@ -36,13 +36,13 @@ if __name__ == '__main__':
 
     number_of_existing_results = 101
     count = 0
-    for file in range(len(chembl_info)):
+    list_of_files = ["data_CHEMBL229.csv", "data_CHEMBL4805.csv", "data_CHEMBL268.csv",
+                     "data_CHEMBL283.csv"]
+    for file in list_of_files:
         #  list_of_files_done: [data_CHEMBL3286(size, 1002; repetition rate: 0.04),
         #  "data_CHEMBL5071.csv" size 1002, repetition 0.019,]
-        list_of_files = [ "data_CHEMBL229.csv", "data_CHEMBL4805.csv", "data_CHEMBL268.csv",
-                         "data_CHEMBL283.csv"]
         # a list of low repetition rate
-        if chembl_info["File name"][file] not in list_of_files: continue
+
 
         # TODO: may need to change the way of getting parent directory if this does not work on windows
         print("On Dataset No.", count, ", ", chembl_info["File name"][file])
