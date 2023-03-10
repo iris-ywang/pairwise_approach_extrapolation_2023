0c15ca0810fe2555078c068a2eb8da564bb7af72
iris-ywang <iris_ywang@outlook.com>
2023-03-10 12:21:18 +0000

    Meta Chembl Trial 4: PDR method, n_features > 1000

--- code/run.py
+++ code/run.py
@@ -15,34 +15,44 @@ def get_chembl_info():
         "N(sample)",
         "N(features)",
         "OpenML ID"])
-    connection = "/code/input/qsar_data_meta/small_feature43_6337_24848/"
+    connection = "/code/input/qsar_data_meta/24850-27000/"
     file_names = os.listdir(os.getcwd() + connection)
     for filename in file_names:
+        if ".csv" not in filename:
+            continue
         dataset = pd.read_csv(os.getcwd() + connection + filename, index_col=None)
         n_samples, n_features = dataset.shape
         n_features -= 1
-        if (dataset.iloc[:, 1:].nunique() <= 10).any():
-            print(filename)
-            print(dataset.iloc[:, 1:].nunique())
+        # if (dataset.iloc[:, 1:].nunique() <= 10).any():
+        #     print(filename)
+        #     print(dataset.iloc[:, 1:].nunique())
+        chembl_id = filename.split("-Nf-")[0].split("CHEMBL")[1]
+        if chembl_id.endswith(str(n_features)):
+            chembl_id = chembl_id[:-len(str(n_features))]
+
         chembl_info = chembl_info.append({
             "File name": filename,
-            "Chembl ID": filename.replace("QSAR-DATASET-FOR-DRUG-TARGET-CHEMBL", "").replace(".csv", ""),
+            "Chembl ID": chembl_id,
             "N(sample)": n_samples,
             "N(features)": n_features,
-            "OpenML ID": "6337-24848"},
+            "OpenML ID": "24850-27000"},
             ignore_index=True
         )
-
+    return chembl_info
 
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
 
-    chembl_info = pd.read_csv("input//chembl_reg_info.csv").sort_values(by=["N(sample)"])
+    chembl_info_all = pd.read_csv("input//chembl_reg_info.csv")
+    chembl_info = chembl_info_all[chembl_info_all["OpenML ID"] == "24850-27000"]
+    chembl_info = chembl_info[chembl_info_all["N(features)"] > 1000]
+
+    chembl_info = chembl_info.sort_values(by=["N(sample)"])
     chembl_info = chembl_info[chembl_info["N(sample)"] >= 30]
 
     try:
-        existing_results = np.load("extrapolation_kfold_cv_reg_trial3.npy")
+        existing_results = np.load("extrapolation_kfold_cv_reg_trial4.npy")
         existing_count = len(existing_results)
         all_metrics = list(existing_results)
     except:
@@ -51,16 +61,16 @@ if __name__ == '__main__':
         all_metrics = []
 
     try:
-        _ = np.load("extrapolation_temporary_dataset_count_reg_trial3.npy")
+        _ = np.load("extrapolation_temporary_dataset_count_reg_trial4.npy")
     except:
-        np.save("extrapolation_temporary_dataset_count_reg_trial3.npy", [0])
+        np.save("extrapolation_temporary_dataset_count_reg_trial4.npy", [0])
 
     count = 0
     for file in range(len(chembl_info)):
         count += 1
         if count <= existing_count:
             continue
-        connection = "/input/qsar_data_meta/small_feature43_6337_24848/"
+        connection = "/input/qsar_data_meta/24850-27000/"
         filename = chembl_info.iloc[file]["File name"]
         print(datetime.now(), " -- ", "On Dataset No.", count, ", ", filename)
 
@@ -73,4 +83,4 @@ if __name__ == '__main__':
         all_metrics.append(metrics[0])
         print(datetime.now(), " -- ")
         print(np.nanmean(metrics[0], axis=0))
-        np.save("extrapolation_kfold_cv_reg_trial3", np.array(all_metrics))
+        np.save("extrapolation_kfold_cv_reg_trial4", np.array(all_metrics))
