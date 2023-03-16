4e607ba8df0c2d03402a72bfaa821c8b416b19c0
iris-ywang <iris_ywang@outlook.com>
2023-03-16 22:20:18 +0000

    RERUN Chembl 10fCV, RF, with ranking by c2+c3 test pairs

--- code/build_model.py
+++ code/build_model.py
@@ -1,7 +1,6 @@
 import numpy as np
-import pickle
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import LinearRegression, LogisticRegression
+
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, ndcg_score
 from scipy.stats import spearmanr, kendalltau
 from extrapolation_evaluation import EvaluateAbilityToIdentifyTopTestSamples
@@ -74,7 +73,7 @@ def metrics_evaluation(y_true, y_predict):
 
 
 def performance_standard_approach(all_data, percentage_of_top_samples):
-    sa_model, y_SA = build_ml_model(LinearRegression(n_jobs=-1), all_data['train_set'], all_data['test_set'])
+    sa_model, y_SA = build_ml_model(RandomForestRegressor(n_jobs=-1, random_state=1), all_data['train_set'], all_data['test_set'])
     y_pred_all = np.array(all_data["y_true"])
     y_pred_all[all_data["test_ids"]] = y_SA
 
@@ -91,11 +90,11 @@ def performance_pairwise_approach(all_data, percentage_of_top_samples, batch_siz
 
         train_pairs_for_sign = np.array(train_pairs_batch)
         train_pairs_for_sign[:, 0] = np.sign(train_pairs_for_sign[:, 0])
-        rfc = LogisticRegression(n_jobs=-1, random_state=1, max_iter=1000)
+        rfc = RandomForestClassifier(n_jobs=-1, random_state=1)
         rfc = build_ml_model(rfc, train_pairs_for_sign)
 
         train_pairs_for_abs = np.absolute(train_pairs_batch)
-        rfr = LinearRegression(n_jobs=-1)
+        rfr = RandomForestRegressor(n_jobs=-1, random_state=1)
         rfr = build_ml_model(rfr, train_pairs_for_abs)
 
     else:
@@ -111,11 +110,11 @@ def performance_pairwise_approach(all_data, percentage_of_top_samples, batch_siz
 
             train_pairs_for_sign = np.array(train_pairs_batch)
             train_pairs_for_sign[:, 0] = np.sign(train_pairs_for_sign[:, 0])
-            rfc = LogisticRegression(n_jobs=-1, random_state=1, warm_start=True, max_iter=1000)
+            rfc = RandomForestRegressor(n_jobs=-1, random_state=1, warm_start=True)
             rfc = build_ml_model(rfc, train_pairs_for_sign)
 
             train_pairs_for_abs = np.absolute(train_pairs_batch)
-            rfr = LinearRegression(n_jobs=-1)
+            rfr = RandomForestRegressor(n_jobs=-1, random_state=1, warm_start=True)
             rfr = build_ml_model(rfr, train_pairs_for_abs)
 
             rfc.n_estimators += 100
@@ -137,8 +136,25 @@ def performance_pairwise_approach(all_data, percentage_of_top_samples, batch_siz
         Y_pa_c2_dist += list(rfr.predict(np.absolute(test_pairs_batch[:, 1:])))
         Y_pa_c2_true += list(test_pairs_batch[:,0])
 
+    c3_test_pair_ids = all_data["c3_test_pair_ids"]
+    number_test_batches = len(c3_test_pair_ids) // batch_size
+    if number_test_batches < 1: number_test_batches = 0
+    Y_pa_c3_sign = []
+    Y_pa_c3_true = []
+    for test_batch in range(number_test_batches + 1):
+        if test_batch != number_test_batches:
+            test_pair_id_batch = c3_test_pair_ids[
+                                 test_batch * batch_size: (test_batch + 1) * batch_size]
+        else:
+            test_pair_id_batch = c3_test_pair_ids[test_batch * batch_size:]
+        test_pairs_batch = paired_data_by_pair_id(all_data["train_test"], test_pair_id_batch)
+        Y_pa_c3_sign += list(rfc.predict(test_pairs_batch[:, 1:]))
+        Y_pa_c3_true += list(test_pairs_batch[:, 0])
+
     Y_c2_sign_and_abs_predictions = dict(zip(all_data["c2_test_pair_ids"], np.array([Y_pa_c2_dist, Y_pa_c2_sign]).T))
-    y_ranking = rating_trueskill(Y_pa_c2_sign, all_data["c2_test_pair_ids"], all_data["y_true"])
+    y_ranking = rating_trueskill(Y_pa_c2_sign + Y_pa_c3_sign,
+                                 all_data["c3_test_pair_ids"] + all_data["c2_test_pair_ids"],
+                                 all_data["y_true"])
 
     metrics = EvaluateAbilityToIdentifyTopTestSamples(percentage_of_top_samples, all_data["y_true"],
                                                       y_ranking, all_data).run_evaluation(Y_c2_sign_and_abs_predictions)
@@ -146,10 +162,10 @@ def performance_pairwise_approach(all_data, percentage_of_top_samples, batch_siz
 
 
 def run_model(data, current_dataset_count, percentage_of_top_samples):
-    temporary_file_dataset_count = int(np.load("extrapolation_temporary_dataset_count_lr.npy"))
+    temporary_file_dataset_count = int(np.load("extrapolation_temporary_dataset_count_rf_rerun.npy"))
 
     if current_dataset_count == temporary_file_dataset_count:
-        existing_iterations = np.load("extrapolation_kfold_cv_all_data_temporary_lr.npy")
+        existing_iterations = np.load("extrapolation_kfold_cv_all_data_temporary_rf_rerun.npy")
         existing_count = len(existing_iterations)
         metrics = list(existing_iterations)
     else:
@@ -164,7 +180,7 @@ def run_model(data, current_dataset_count, percentage_of_top_samples):
         metric_pa, rfc_pa, rfr_pa = performance_pairwise_approach(datum, percentage_of_top_samples)
         metrics.append([metric_sa, metric_pa])
 
-        np.save("extrapolation_temporary_dataset_count_lr.npy", [current_dataset_count])
-        np.save("extrapolation_kfold_cv_all_data_temporary_lr.npy", np.array(metrics))
+        np.save("extrapolation_temporary_dataset_count_rf_rerun.npy", [current_dataset_count])
+        np.save("extrapolation_kfold_cv_all_data_temporary_rf_rerun.npy", np.array(metrics))
 
     return np.array([metrics])
